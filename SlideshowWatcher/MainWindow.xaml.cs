93efabcaa97a1bc9d48ae298f6fa8debbb0e99d7
Peter Bozovic <pbozovic@gmail.com>
2018-03-27 15:20:02 +0200

    Prototype Slideshow

--- SlideshowWatcher/MainWindow.xaml.cs
+++ SlideshowWatcher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -11,9 +12,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SlideshowWatcher
 {
@@ -22,14 +25,33 @@ namespace SlideshowWatcher
     /// </summary>
     public partial class MainWindow : Window
     {
-        FileSystemWatcher watcher;
+
+        private DispatcherTimer timerImageChange;
+        private Image[] ImageControls;
+        private List<ImageSource> Images = new List<ImageSource>();
+        private static string[] ValidImageExtensions = new[] { ".png", ".jpg", ".jpeg", ".bmp", ".gif" };
+        private static string[] TransitionEffects = new[] { "Fade" };
+        private string TransitionType, strImagePath = "";
+        private int CurrentSourceIndex, CurrentCtrlIndex, EffectIndex = 0, IntervalTimer = 1;
+        private FileSystemWatcher watcher;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            //Initialize Image control, Image directory path and Image timer.
+            IntervalTimer = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalTime"]);
+            strImagePath = ConfigurationManager.AppSettings["ImagesPath"];
+            ImageControls = new[] { myImage, myImage2 };
+
+            LoadImageFolder(strImagePath);
+
+            timerImageChange = new DispatcherTimer();
+            timerImageChange.Interval = new TimeSpan(0, 0, IntervalTimer);
+            timerImageChange.Tick += new EventHandler(timerImageChange_Tick);
+
             watcher = new FileSystemWatcher();
-            watcher.Path = "D:\\fete.moulin";
+            watcher.Path = ConfigurationManager.AppSettings["ImagesPath"];
             watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size;
             watcher.Changed += Watcher_Event;
             watcher.Created += Watcher_Event;
@@ -41,6 +63,107 @@ namespace SlideshowWatcher
         private void Watcher_Event(object sender, FileSystemEventArgs e)
         {
             Debug.WriteLine($"{ DateTime.Now.ToString("s") } : {e.ChangeType} - {e.FullPath}");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PlaySlideShow();
+            timerImageChange.IsEnabled = true;
+        }
+
+        private bool fullScreen;
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(fullScreen)
+            {
+                WindowStyle = WindowStyle.SingleBorderWindow;
+                WindowState = WindowState.Normal;
+                fullScreen = false;
+            } else
+            {
+                WindowStyle = WindowStyle.None;
+                WindowState = WindowState.Maximized;
+                fullScreen = true;
+            }
+
+        }
+
+        private void LoadImageFolder(string folder)
+        {
+            ErrorText.Visibility = Visibility.Collapsed;
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            if (!System.IO.Path.IsPathRooted(folder))
+                folder = System.IO.Path.Combine(Environment.CurrentDirectory, folder);
+            if (!System.IO.Directory.Exists(folder))
+            {
+                ErrorText.Text = "The specified folder does not exist: " + Environment.NewLine + folder;
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+            Random r = new Random();
+            var sources = from file in new System.IO.DirectoryInfo(folder).GetFiles().AsParallel()
+                          where ValidImageExtensions.Contains(file.Extension, StringComparer.InvariantCultureIgnoreCase)
+                          orderby r.Next()
+                          select CreateImageSource(file.FullName, true);
+            var files = from file in new System.IO.DirectoryInfo(folder).GetFiles().AsParallel()
+                          where ValidImageExtensions.Contains(file.Extension, StringComparer.InvariantCultureIgnoreCase)
+                          orderby r.Next()
+                          select file.FullName;
+            Images.Clear();
+            Images.AddRange(sources);
+            sw.Stop();
+            Console.WriteLine("Total time to load {0} images: {1}ms", Images.Count, sw.ElapsedMilliseconds);
+        }
+
+        private ImageSource CreateImageSource(string file, bool forcePreLoad)
+        {
+            if (forcePreLoad)
+            {
+                var src = new BitmapImage();
+                src.BeginInit();
+                src.UriSource = new Uri(file, UriKind.Absolute);
+                src.CacheOption = BitmapCacheOption.OnLoad;
+                src.EndInit();
+                src.Freeze();
+                return src;
+            }
+            else
+            {
+                var src = new BitmapImage(new Uri(file, UriKind.Absolute));
+                src.Freeze();
+                return src;
+            }
+        }
+
+        private void timerImageChange_Tick(object sender, EventArgs e)
+        {
+            PlaySlideShow();
+        }
+
+        private void PlaySlideShow()
+        {
+            try
+            {
+                if (Images.Count == 0)
+                    return;
+                var oldCtrlIndex = CurrentCtrlIndex;
+                CurrentCtrlIndex = (CurrentCtrlIndex + 1) % 2;
+                CurrentSourceIndex = (CurrentSourceIndex + 1) % Images.Count;
+
+                Image imgFadeOut = ImageControls[oldCtrlIndex];
+                Image imgFadeIn = ImageControls[CurrentCtrlIndex];
+                ImageSource newSource = Images[CurrentSourceIndex];
+                imgFadeIn.Source = newSource;
+
+                TransitionType = TransitionEffects[EffectIndex].ToString();
+
+                Storyboard StboardFadeOut = (Resources[string.Format("{0}Out", TransitionType.ToString())] as Storyboard).Clone();
+                StboardFadeOut.Begin(imgFadeOut);
+                Storyboard StboardFadeIn = Resources[string.Format("{0}In", TransitionType.ToString())] as Storyboard;
+                StboardFadeIn.Begin(imgFadeIn);
+            }
+            catch (Exception ex) { }
         }
     }
 }
