920e0d24981418029173456c1406baf0f7e0095e
Peter Bozovic <pbozovic@gmail.com>
2018-03-27 14:03:01 +0200

    Prototype FileSysteWatcher

---
+++ SlideshowWatcher/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlideshowWatcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileSystemWatcher watcher;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            watcher = new FileSystemWatcher();
+            watcher.Path = "D:\\fete.moulin";
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size;
+            watcher.Changed += Watcher_Event;
+            watcher.Created += Watcher_Event;
+            watcher.Deleted += Watcher_Event;
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void Watcher_Event(object sender, FileSystemEventArgs e)
+        {
+            Debug.WriteLine($"{ DateTime.Now.ToString("s") } : {e.ChangeType} - {e.FullPath}");
+        }
+    }
+}
